6cb1407e8cbc681df54f6ab15511b9e32127d55c
patpat261 <patrick.malapit26@gmail.com>
2025-01-09 16:22:45 +0100

    fix everything

--- frontend/src/services/movieService.js
+++ frontend/src/services/movieService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export class MovieService {
 
-    apiUrl = "http://localhost:8080/api/movie/movies/"
+    apiUrl = "http://movie-service:8080/api/movie/movies/"
 
     getAllDisplayingMovies() {
         return axios.get(this.apiUrl + "displayingMovies");
--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export class UserService {
 
-    apiUrl = "http://localhost:8080/api/user/users/"
+    apiUrl = "http://host.docker.internal:8080/api/user/users/"
 
     addCustomer(customer) {
         return axios.post(this.apiUrl + "add", customer);
     }
 
     login(loginDto) {
-        return axios.post("http://localhost:8080/api/user/auth/login", loginDto);
+        return axios.post("http://172.26.0.11:8080/api/user/auth/login", loginDto);
     }
 }
